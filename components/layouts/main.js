84d2272d23ce7fc17430e2bc0cfaa6911dafec3f
QiWen Wang <isqiwen@gmail.com>
2020-06-21 02:48:46 +0800

    build own

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -12,7 +12,7 @@ export default ({ children, headerActive }) => {
     <main>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <title>Guillermo Rauch's blog</title>
+        <title>Wang QiWen's blog</title>
       </Head>
 
       <style jsx global>
