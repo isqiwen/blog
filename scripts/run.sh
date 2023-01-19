4f2234248cf71426f99bfadb7a2acdcbb1c9f451
Wang QiWen <qiwen.wang@marvelstonemed.com>
2023-01-19 16:30:21 +0800

    update scripts for production

--- scripts/run.sh
+++ scripts/run.sh
@@ -4,4 +4,24 @@ scriptsPath=$(dirname $(readlink -f $0))
 rootPath="${scriptsPath}/.."
 
 source ${rootPath}/.pyvenv/bin/activate
-pc run
+
+runProduction=0
+
+while getopts "p" arg
+do
+    case $arg in
+        p)
+            runProduction=1
+            ;;
+        ?)
+            echo "unknown argument"
+            exit 1
+            ;;
+    esac
+done
+
+if [[ $runProduction -eq 1 ]]; then
+    pc run --env prod
+else
+    pc run
+fi
